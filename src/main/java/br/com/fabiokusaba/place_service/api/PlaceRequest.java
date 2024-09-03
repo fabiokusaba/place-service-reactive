ecb202938d5dcff6f3db274079bfdb70249fed4e
fabiokusaba <fabinhokb44@gmail.com>
2024-09-03 20:29:54 -0300

    added validation on request

--- src/main/java/br/com/fabiokusaba/place_service/api/PlaceRequest.java
+++ src/main/java/br/com/fabiokusaba/place_service/api/PlaceRequest.java
@@ -1,8 +1,10 @@
 package br.com.fabiokusaba.place_service.api;
 
+import jakarta.validation.constraints.NotBlank;
+
 // DTOs -> ao invés de você pegar um objeto de domínio e cruzar a aplicação inteira com ele, ou seja, a camada web
 // a camada de serviço, a camada de repositório, você cria um DTO para conter apenas as propriedades que você quer
 // expor para o mundo exterior
-public record PlaceRequest(String name, String state) {
+public record PlaceRequest(@NotBlank String name, @NotBlank String state) {
 
 }
