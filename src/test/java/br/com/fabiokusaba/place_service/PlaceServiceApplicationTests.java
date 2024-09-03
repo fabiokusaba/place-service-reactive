d6c7698590647348b7e04031af7c4fa7602f4030
fabiokusaba <fabinhokb44@gmail.com>
2024-09-03 07:16:38 -0300

    Initial commit

---
+++ src/test/java/br/com/fabiokusaba/place_service/PlaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabiokusaba.place_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
