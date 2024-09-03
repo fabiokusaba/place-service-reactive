f9be9a1874fca3b76f1e2f8c10c08b96bee3d020
fabiokusaba <fabinhokb44@gmail.com>
2024-09-03 19:34:21 -0300

    Enabled R2dbcAuditing

--- src/main/java/br/com/fabiokusaba/place_service/config/PlaceConfig.java
+++ src/main/java/br/com/fabiokusaba/place_service/config/PlaceConfig.java
@@ -2,12 +2,14 @@ package br.com.fabiokusaba.place_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 import br.com.fabiokusaba.place_service.domain.PlaceRepository;
 import br.com.fabiokusaba.place_service.domain.PlaceService;
 
 // Classe de configuração do container de injeção do Spring
 @Configuration
+@EnableR2dbcAuditing
 public class PlaceConfig {
 
     // Criação do Bean que vai nos retornar um PlaceService e aqui eu monto qual seria a configuração do PlaceService
--- src/main/java/br/com/fabiokusaba/place_service/domain/Place.java
+++ src/main/java/br/com/fabiokusaba/place_service/domain/Place.java
@@ -2,12 +2,20 @@ package br.com.fabiokusaba.place_service.domain;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
 // Uma dica é trabalharmos com records porque torna o nosso código menos verboso quando estamos trabalhando com pojos que só
 // possuem atributos, getters e setters
 // Precisamos analisar a suas limitações já que records são imutáveis o que pode ser bom ou pode ser ruim, mas para o nosso
 // caso de uma aplicação CRUD é bem útil
+// Essas duas anotações que colocamos CreatedDate e LastModifiedDate vão preencher automaticamente a informação da criação e
+// atualização do objeto não vou mais precisar me preocupar com isso porque o Spring vai preencher pra mim, para isso a única
+// coisa que vou precisar fazer é nas minhas configurações habilitar o EnableR2dbcAuditing e com essa auditoria vou ter as
+// informações preenchidas automaticamente quando eu criar o meu objeto pelo framework
 public record Place(
-    Long id, String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt
+    Long id, String name, String slug, String state, 
+    @CreatedDate LocalDateTime createdAt, @LastModifiedDate LocalDateTime updatedAt
 ) {
 
 }
