450bc29acce831d8de1357be5cbae5793a4e69f5
fabiokusaba <fabinhokb44@gmail.com>
2024-09-03 11:41:07 -0300

    created Place controller in web layer

---
+++ src/main/java/br/com/fabiokusaba/place_service/web/PlaceController.java
@@ -0,0 +1,31 @@
+package br.com.fabiokusaba.place_service.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fabiokusaba.place_service.domain.Place;
+import br.com.fabiokusaba.place_service.domain.PlaceService;
+import reactor.core.publisher.Mono;
+
+// Controlador que vai estabelecer os endpoints da nossa API conforme os requisitos estabelecidos
+@RestController
+@RequestMapping("/places")
+public class PlaceController {
+    // Serviço responsável pelas regras de negócio da aplicação
+    private PlaceService placeService;
+
+    // Injeção de dependência via construtor
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    // Rota de criação
+    @PostMapping
+    public ResponseEntity<Mono<Place>> create(Place place){
+        var createdPlace = placeService.create(place);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPlace);
+    }
+}
