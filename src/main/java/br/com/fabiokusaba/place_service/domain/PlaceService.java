1e600f3d0fec1e3b01a173c51d71fd47663af57d
fabiokusaba <fabinhokb44@gmail.com>
2024-09-03 12:09:00 -0300

    updated create method in service layer

--- src/main/java/br/com/fabiokusaba/place_service/domain/PlaceService.java
+++ src/main/java/br/com/fabiokusaba/place_service/domain/PlaceService.java
@@ -1,5 +1,6 @@
 package br.com.fabiokusaba.place_service.domain;
 
+import br.com.fabiokusaba.place_service.api.PlaceRequest;
 import reactor.core.publisher.Mono;
 
 // Responsável pelas regras de negócio da aplicação, o seu papel é usar o repositório que a gente tem
@@ -13,7 +14,11 @@ public class PlaceService {
 
     // Operação de criação, como estamos trabalhando com API reativa a gente nunca retorna a entidade direta a gente retorna o
     // Mono dela
-    public Mono<Place> create(Place place){
+    public Mono<Place> create(PlaceRequest placeRequest){
+        // Criando uma variável place com as propriedades que vieram da minha request
+        var place = new Place(null, placeRequest.name(), placeRequest.slug(), placeRequest.state(), placeRequest.createdAt(), placeRequest.updatedAt());
+        
+        // Ao final, salvamos no banco de dados
         return placeRepository.save(place);
     }
 }
