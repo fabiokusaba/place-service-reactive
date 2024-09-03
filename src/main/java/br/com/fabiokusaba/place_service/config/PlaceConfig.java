16107b2ffcd9663ce36ab9db23bef6c7ba966b07
fabiokusaba <fabinhokb44@gmail.com>
2024-09-03 11:46:09 -0300

    created Place configuration in config layer

---
+++ src/main/java/br/com/fabiokusaba/place_service/config/PlaceConfig.java
@@ -0,0 +1,21 @@
+package br.com.fabiokusaba.place_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.fabiokusaba.place_service.domain.PlaceRepository;
+import br.com.fabiokusaba.place_service.domain.PlaceService;
+
+// Classe de configuração do container de injeção do Spring
+@Configuration
+public class PlaceConfig {
+
+    // Criação do Bean que vai nos retornar um PlaceService e aqui eu monto qual seria a configuração do PlaceService
+    // Se dermos uma olhada ele recebe o PlaceRepository então também vamos precisar injetá-lo e podemos fazer isso
+    // passando ele como parâmetro do método e o Spring fica encarregado de injetá-lo
+    // Desta forma, temos o nosso PlaceService configurado para injeção
+    @Bean
+    PlaceService placeService(PlaceRepository placeRepository){
+        return new PlaceService(placeRepository);
+    }
+}
